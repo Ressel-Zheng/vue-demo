0e22b5ced421a701af0cc8775e33cd3ff2289212
Ressel_Zheng <837104654@qq.com>
2019-03-18 14:01:17 +0800

    first commit

---
+++ src/views/User.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+    <button type="button" @click="reg1()">添加</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "User",
+        methods:{
+            reg1() {
+                this.$router.push('/add')
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
